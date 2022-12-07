096477c9d85b612f55d1c437bf188520e7eb6cbe
Corey <corey.kumler@yahoo.com>
2022-12-06 23:07:38 -0500

    Update cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config()
 
+const cors = require('cors')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -12,6 +13,11 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+
+// Add the cors middleware to your API
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 const inventoryRouter = require('./routes/inventory')
 app.use('/inventory', inventoryRouter)
