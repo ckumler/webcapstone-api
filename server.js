909e058aa9d85e47253d4ac9fbf52da68996c7b6
Corey <corey.kumler@yahoo.com>
2022-12-06 23:08:48 -0500

    Update cors to use env

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 // Add the cors middleware to your API
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.CORS_ORGIN,
 }));
 
 const inventoryRouter = require('./routes/inventory')
